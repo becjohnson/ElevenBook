d083d1b52947a66290362962233f01edd11e4858
Marjorie Sandoval <Marjorie.Sandoval74@gmail.com>
2021-12-01 15:21:07 -0500

    final

---
+++ ElevenBook.Data/Migrations/202112012002143_Final.cs
@@ -0,0 +1,20 @@
+﻿namespace ElevenBook.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Final : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Reply", "CreatedUtc", c => c.DateTimeOffset(nullable: false, precision: 7));
+            DropColumn("dbo.Reply", "CreateUtc");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Reply", "CreateUtc", c => c.DateTimeOffset(nullable: false, precision: 7));
+            DropColumn("dbo.Reply", "CreatedUtc");
+        }
+    }
+}
---
+++ ElevenBook.WebAPI/Controllers/ReplyController.cs
@@ -0,0 +1,72 @@
+﻿using ElevenBook.Models;
+using ElevenBook.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenBook.Controllers
+{
+    [Authorize]
+    public class ReplyController : ApiController
+    {
+        private ReplyService CreateReplyService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var replyService = new ReplyService(userId);
+            return replyService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            ReplyService replyService = CreateReplyService();
+            var reply = replyService.GetReply();
+            return Ok(reply);
+        }
+        public IHttpActionResult Post(ReplyCreate reply)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.CreateReply(reply))
+                return InternalServerError();
+           
+            return Ok();
+        }
+        public IHttpActionResult Get(int id)
+        {
+            ReplyService ReplyService = CreateReplyService();
+            var reply = ReplyService.GetReplyById(id);
+            return Ok(reply);
+        }
+
+        public IHttpActionResult Put(ReplyEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.UpdateReply(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateReplyService();
+
+            if (!service.DeleteReply(id))
+                return InternalServerError();
+            return Ok();
+        }
+
+    }
+}
+
+
