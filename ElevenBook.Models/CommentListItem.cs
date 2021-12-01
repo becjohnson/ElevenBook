aa91a0c6a307af70949cdb68692d5c1010d08d1b
becjohnson <91417720+becjohnson@users.noreply.github.com>
2021-12-01 13:40:32 -0500

    Final Commit

---
+++ ElevenBook.Models/CommentListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ElevenBook.Models
+{
+    public class CommentListItem
+    {
+        public int CommentId
+        {
+            get; set;
+        }
+        [Required]
+        [MaxLength(8000)]
+        [Display(Name = "Comment")]
+        public string Text
+        {
+            get; set;
+        }
+     }
+}
---
+++ ElevenBook.Models/ReplyEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenBook.Models
+{
+    public class ReplyEdit
+    {
+        public int ReplyId { get; set; }
+        [Required]
+        [MaxLength(8000)]
+        [Display(Name = "Reply")]
+        public string Text { get; set; }
+    }
+}
